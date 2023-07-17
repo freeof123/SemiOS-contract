6502125d5c9b2b7e5af7463efc43b550760c4e3b
Allan-Wei <12114666+Allan-Wei@user.noreply.gitee.com>
2023-07-17 15:07:45 +0800

    Fix: `setDaoPriceTemplate` typo

--- test/foundry/unit-tests/D4AProtocolSetter.t.sol
+++ test/foundry/unit-tests/D4AProtocolSetter.t.sol
@@ -435,4 +435,50 @@ contract D4AProtocolSetterTest is DeployHelper {
         assertEq(D4AProtocolReadable(address(protocol)).getCanvasNextPrice(canvasId), 0.5 ether * 128);
         assertEq(D4AProtocolReadable(address(protocol)).getCanvasNextPrice(canvasId2), 0.5 ether);
     }
+
+    function test_RevertIf_setDaoPriceTemplate_ExponentialPriceVariation_priceFactor_less_than_10001() public {
+        hoax(daoCreator.addr);
+        bytes32 daoId = daoProxy.createProject{ value: 0.1 ether }(
+            DaoMetadataParam({
+                startDrb: 1,
+                mintableRounds: 30,
+                floorPriceRank: 0,
+                maxNftRank: 0,
+                royaltyFee: 750,
+                projectUri: "test dao uri",
+                projectIndex: 0
+            }),
+            Whitelist({
+                minterMerkleRoot: bytes32(0),
+                minterNFTHolderPasses: new address[](0),
+                canvasCreatorMerkleRoot: bytes32(0),
+                canvasCreatorNFTHolderPasses: new address[](0)
+            }),
+            Blacklist({ minterAccounts: new address[](0), canvasCreatorAccounts: new address[](0) }),
+            DaoMintCapParam({ daoMintCap: 0, userMintCapParams: new UserMintCapParam[](0) }),
+            DaoETHAndERC20SplitRatioParam({
+                daoCreatorERC20Ratio: 300,
+                canvasCreatorERC20Ratio: 9500,
+                nftMinterERC20Ratio: 3000,
+                daoFeePoolETHRatio: 3000,
+                daoFeePoolETHRatioFlatPrice: 3500
+            }),
+            TemplateParam({
+                priceTemplateType: PriceTemplateType.LINEAR_PRICE_VARIATION,
+                priceFactor: 0.01 ether,
+                rewardTemplateType: RewardTemplateType.EXPONENTIAL_REWARD_ISSUANCE,
+                rewardDecayFactor: 12_600,
+                isProgressiveJackpot: true
+            }),
+            0
+        );
+        hoax(daoCreator.addr);
+        D4AProtocolSetter(address(protocol)).setDaoPriceTemplate(
+            daoId, PriceTemplateType.LINEAR_PRICE_VARIATION, 10_000
+        );
+        vm.expectRevert();
+        D4AProtocolSetter(address(protocol)).setDaoPriceTemplate(
+            daoId, PriceTemplateType.EXPONENTIAL_PRICE_VARIATION, 10_000
+        );
+    }
 }
