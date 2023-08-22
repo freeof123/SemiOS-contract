a08744c999d2bd897625b40bb34665802858b4c9
wangjinning <2571763814@qq.com>
2023-08-22 13:49:49 +0800

    Update BaiscDaoUnlocker.t.sol

--- test/foundry/unit-tests/BasicDaoUnlocker.t.sol
+++ test/foundry/unit-tests/BasicDaoUnlocker.t.sol
@@ -1,35 +1,46 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
-import "forge-std/Test.sol";
+import { Test, console } from "forge-std/Test.sol";
 
 import { BasicDaoUnlocker } from "contracts/BasicDaoUnlocker.sol";
-import { LibBitmap } from "solady/utils/LibBitmap.sol";
-import { ProtoDaoProtocolSetter } from "../../../contracts/ProtoDaoProtocolSetter.sol";
-import { DeployHelper } from "../../foundry/utils/DeployHelper.sol";
+import { DeployHelper } from "test/foundry/utils/DeployHelper.sol";
 import "contracts/interface/D4AStructs.sol";
+import { D4AFeePool } from "contracts/feepool/D4AFeePool.sol";
+import { ID4AProtocolReadable } from "contracts/interface/ID4AProtocolReadable.sol";
 
-import { IProtoDaoProtocol } from "contracts/interface/IProtoDaoProtocol.sol";
+import { IPDProtocolReadable } from "contracts/interface/IPDProtocolReadable.sol";
+import { IPDBasicDao } from "contracts/interface/IPDBasicDao.sol";
 
-contract BasicDaoUnlockerTest is Test, ProtoDaoProtocolSetter, DeployHelper {
-    // 初始化Dao
+contract BasicDaoUnlockerTest is Test, DeployHelper {
+    bytes32 basicDaoId1;
+    bytes32 basicDaoId2;
+    address basicDaoFeePool1;
+    address basicDaoFeePool2;
+    BasicDaoUnlocker public unlocker;
+    address public unlockerPROTOCOL;
+
     function setUp() public {
         setUpEnv();
 
-        DeployHelper.CreateDaoParam memory createDaoParam;
-        bytes32 daoId = _
+        DeployHelper.CreateDaoParam memory basicDaoParam1;
+        DeployHelper.CreateDaoParam memory basicDaoParam2;
 
+        basicDaoParam1.daoUri = "basic dao1";
+        basicDaoParam2.daoUri = "basic dao2";
 
+        basicDaoId1 = _createDao(basicDaoParam1);
+        basicDaoId2 = _createDao(basicDaoParam2);
 
-
-
+        basicDaoFeePool1 = ID4AProtocolReadable(address(protocol)).getDaoFeePool(basicDaoId1);
+        basicDaoFeePool2 = ID4AProtocolReadable(address(protocol)).getDaoFeePool(basicDaoId2);
     }
 
-    // 验证  流水不足2ETH时无法解锁/流水超过2ETH时可以解锁
-    function test_UnlockStatus() public { }
+    // verify  Can't unlock when the turnover <2ETH.
+    function test_UnlockStatus() public view { }
 
-    // 验证  对流水不足的Dao增加到2ETH，后可以解锁
-    function test_CanUnlockAfterRaseTo2ETH() public { }
+    // verify  When the turnover raise to 2ETH,can unlock the DAO.
+    function test_CanUnlockAfterRaiseTo2ETH() public { }
 
     //
 }
