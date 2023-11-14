c12a84e173968247f0b80ee4fbcef110cb93009c
wangjinning <2571763814@qq.com>
2023-11-14 14:19:54 +0800

    test

--- test/foundry/unit-tests/FundingDaoParamSet.t.sol
+++ test/foundry/unit-tests/FundingDaoParamSet.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import { DeployHelper } from "test/foundry/utils/DeployHelper.sol";
 import { PDProtocolSetter } from "contracts/PDProtocolSetter.sol";
 import "contracts/interface/D4AStructs.sol";
@@ -45,24 +47,24 @@ contract FundingDaoParamSet is DeployHelper {
         // 在上面创建的MainDAO基础上创建一个ContinuousDAO
         param.canvasId = keccak256(abi.encode(daoCreator2.addr, block.timestamp + 1));
         param.daoUri = "continuous dao";
-        param.existDaoId = bytes32(0);
-        param.isBasicDao = true;
+        param.existDaoId = daoId; // MainDAO的id
+        param.isBasicDao = false; // 不是MainDAO，即为SubDAOß
         param.uniPriceModeOff = true;
         param.topUpMode = false;
 
         bytes32 continuousDaoId = _createDaoForFunding(param, daoCreator2.addr);
 
-        // 修改ContinuousDAO的参数
+        // 用ContinuousDAO创建者的地址尝试修改ContinuousDAO的参数,无法成功追加DAO Token
         vars.daoId = continuousDaoId;
-
+        vars.initialTokenSupply = 2 ether;
+        address daoToken = protocol.getDaoToken(daoId);
         hoax(daoCreator2.addr);
         protocol.setDaoParamsFunding(vars);
+        assertEq(IERC20(daoToken).balanceOf(protocol.getDaoAssetPool(continuousDaoId)), 0 ether);
 
-        // 用非创建者的地址尝试修改ContinuousDAO的参数
-        console2.log(daoCreator.addr);
-        console2.log(daoCreator2.addr);
-
+        // 用MainDAO创建者的地址尝试修改ContinuousDAO的参数，可以成功追加DAO Token
         hoax(daoCreator.addr);
         protocol.setDaoParamsFunding(vars);
+        assertEq(IERC20(daoToken).balanceOf(protocol.getDaoAssetPool(continuousDaoId)), 2 ether);
     }
 }
